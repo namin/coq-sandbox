b4928b46184bce13cebcdeba6ffaab8790a59ef1
Nada Amin <namin@alum.mit.edu>
2012-07-19 23:17:49 +0200

    more properties of eq

--- eq.v
+++ eq.v
@@ -16,3 +16,9 @@ Definition eq_sym' (A:Type)(x y:A)(h : x=y) : y=x :=
 
 Definition eq_trans' (A:Type)(x y z:A)(h : x=y)(g : y=z) : x=z :=
   eq_ind y (fun z => x=z) h z g.
+
+Definition eq_subst' (A:Type)(B: A -> Prop)(x y:A)(h : x=y)(g : B x) : B y :=
+  eq_ind x (fun z => B z) g y h.
+
+Definition eq_resp' (A:Type)(B: Prop)(x y:A)(h : x=y)(g: A -> B) : g x = g y :=
+  eq_ind x (fun z => g x = g z) (refl_equal (g x)) y h.
