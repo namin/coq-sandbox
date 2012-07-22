7a2e159422d7d9dcf04841c6c1bb1519721cc59e
Nada Amin <namin@alum.mit.edu>
2012-07-22 08:29:02 +0200

    one more ex, yuck

--- Cat.v
+++ Cat.v
@@ -1,12 +1,13 @@
 Require Import ConCaT.CATEGORY_THEORY.CATEGORY.Category.
 Require Import ConCaT.CATEGORY_THEORY.CATEGORY.Dual.
 Require Import ConCaT.CATEGORY_THEORY.CATEGORY.SET.
+Require Import ConCaT.CATEGORY_THEORY.CATEGORY.PROD.
 Require Import ConCaT.CATEGORY_THEORY.FUNCTOR.Functor.
 Require Import ConCaT.CATEGORY_THEORY.FUNCTOR.Dual_Functor.
+Require Import ConCaT.SETOID.Setoid.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
-
 
 (**
 
@@ -141,3 +142,97 @@ Check x2a_Fun.
 x2a_Fun
      : SETop -> Functor SETop SET
 *)
+
+(**
+Homework 1, problem 3
+
+(a) Show that Hom is a functor Hom : Cop x C -> Set.
+
+(b) Show that the slice category construction determines a functor
+    C/(-) : C -> Cat by composition
+
+**)
+
+Section hw1ex3a.
+
+Variables (Cat : Category).
+
+Let CatOp := Dual Cat.
+
+Let IN := PROD CatOp Cat.
+
+Section fundef.
+
+  Definition homfun_ob(p: IN): SET := @Hom Cat ((Ob_l p): Cat) ((Ob_r p): Cat).
+
+  Section mapdef.
+    Variables (p q : IN).
+
+    Section mordef.
+      Variable f : p --> q.
+      Let A: Cat := Ob_l p.
+      Let B: Cat := Ob_r p.
+      Let C: Cat := Ob_l q.
+      Let D: Cat := Ob_r q.
+      Let f1: @Hom Cat C A := Hom_l f.
+      Let f2: @Hom Cat B D := Hom_r f.
+      Definition homfun_mor1(g: @Hom Cat A B) :=
+        f1 o g o f2.
+
+      Lemma homfun_map_law1 : Map_law homfun_mor1.
+      Proof.
+        unfold Map_law, homfun_mor1 in |- *.
+        intros g h H.
+        apply Comp_l. apply Comp_r.
+        assumption.
+      Qed.
+      Canonical Structure homfun_mor : Map (homfun_ob p) (homfun_ob q) :=
+        homfun_map_law1.
+    End mordef.
+
+    Lemma homfun_map_law : Map_law homfun_mor.
+    Proof.
+      unfold Map_law in |- *; simpl in |- *.
+      unfold homfun_mor. unfold Ext. unfold homfun_ob. simpl.
+      unfold homfun_mor1.
+      intros f g H h.
+      inversion H.
+      apply Comp_lr; try assumption.
+      apply Comp_l; assumption.
+    Qed.
+    Canonical Structure homfun_map := Build_Map homfun_map_law.
+  End mapdef.
+
+  Lemma homfun_comp_law : @Fcomp_law IN SET homfun_ob homfun_map.
+  Proof.
+    unfold Fcomp_law. simpl.
+    intros X Y Z f g.
+    unfold homfun_map. simpl.
+    unfold homfun_mor, homfun_ob.
+    unfold Ext. simpl. intros h. simpl.
+    unfold homfun_mor1.
+    simpl. unfold Comp. unfold Cat_comp. simpl.
+    (* stuck *)
+    Admitted.
+  Lemma homfun_id_law : Fid_law homfun_map.
+  Proof.
+    unfold Fid_law. simpl.
+    unfold Ext. simpl.
+    unfold Id_fun. simpl.
+    unfold homfun_mor1, homfun_ob.
+    intros b f. unfold Id_PROD. simpl.
+    (* stuck *)
+    Admitted.
+
+  Canonical Structure hom_Fun := Build_Functor homfun_comp_law homfun_id_law.
+
+End fundef.
+End hw1ex3a.
+
+Check hom_Fun.
+(* ===>
+hom_Fun
+     : forall Cat : Category, Functor (PROD (Dual Cat) Cat) SET
+*)
+
+
