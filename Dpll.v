64d10c2a784eec9233f6c621321ed086a51e5a16
Nada Amin <namin@alum.mit.edu>
2025-01-18 20:51:26 -0500

    Update Dpll.v with cleaned up header

--- Dpll.v
+++ Dpll.v
@@ -1,13 +1,16 @@
-(**
-
-Last exercise from http://adam.chlipala.net/cpdt/html/Subset.html
+(*
+Last exercise from the old http://adam.chlipala.net/cpdt/html/Subset.html,
+see https://web.archive.org/web/20110821195326/http://adam.chlipala.net/cpdt/html/Subset.html
 Also see older more detailed version of similar homework at http://adam.chlipala.net/itp/hw/HW6.html
 
-Implement the DPLL satisfiability decision procedure for boolean formulas in conjunctive normal form, with a dependent type that guarantees its correctness.  An example of a reasonable type for this function would be [forall f : formula, {truth : tvals | formulaTrue truth f} + {][forall truth, ~ formulaTrue truth f}].  Implement at least %``%#"#the basic backtracking algorithm#"#%''% as defined here:
-  %\begin{center}\url{http://en.wikipedia.org/wiki/DPLL_algorithm}\end{center}%
-  #<blockquote><a href="http://en.wikipedia.org/wiki/DPLL_algorithm">http://en.wikipedia.org/wiki/DPLL_algorithm</a></blockquote>#
-It might also be instructive to implement the unit propagation and pure literal elimination optimizations described there or some other optimizations that have been used in modern SAT solvers.
-
+Implement the DPLL satisfiability decision procedure for boolean formulas in conjunctive normal form,
+with a dependent type that guarantees its correctness.
+An example of a reasonable type for this function would be
+[forall f : formula, {truth : tvals | formulaTrue truth f} + {][forall truth, ~ formulaTrue truth f}].
+Implement at least %``%#"#the basic backtracking algorithm#"#%''% as defined here:
+  http://en.wikipedia.org/wiki/DPLL_algorithm
+It might also be instructive to implement the unit propagation and pure literal elimination optimizations described there
+or some other optimizations that have been used in modern SAT solvers.
 *)
 
 Require Import Arith Bool List.
